7067eef7c6a3faa50a553c885021302b47db13b4
JacesChrist <44580832+MatteoStoisa@users.noreply.github.com>
2020-12-12 11:19:01 +0100

    uploaded from acordi96 repo

---
+++ Participant.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Sam on 19/apr/2020.
+//
+
+#include "Headers/Participant.h"
+
+
+int Participant::getId() const {
+    return _siteId;
+}
+
+void Participant::setSiteId(int edId) {
+    this->_siteId = edId;
+}
+
+void Participant::setUsername(std::string userName) {
+    this->username = std::move(userName);
+}
+
+std::string Participant::getUsername() {
+    return this->username;
+}
+
+const std::string &Participant::getCurrentFile() const {
+    return currentFile;
+}
+
+void Participant::setCurrentFile(const std::string &currentFile) {
+    Participant::currentFile = currentFile;
+}
+
+const std::string &Participant::getColor() const {
+    return color;
+}
+
+void Participant::setColor(const std::string &color) {
+    Participant::color = color;
+}
